db07a4f8c91dea8df547d02d95f82fee1b56ed34
solvery@163.com <solvery@163.com>
2016-08-24 10:38:19 +0800

    matlab_image_process

--- convert_to_matlab_read_1.m
+++ convert_to_matlab_read_1.m
@@ -1,7 +1,6 @@
 
-data_in_hex = textread('image_data_in.txt', '%s');  
+data_in_hex_p1 = textread('image_data_in_p1.txt', '%s');  
+data_in_hex_p1 = textread('image_data_in_p1.txt', '%s');  
+data_in_hex_p1 = textread('image_data_in_p1.txt', '%s');  
 data_in_dec = uint8(hex2dec(data_in_hex));
-length(data_in_dec)
-img_in = reshape(
-Q = reshape(img_in_dec, 128, 128);  
-imshow(Q);  
+
--- convert_to_modelsim_read_1.m
+++ convert_to_modelsim_read_1.m
@@ -3,7 +3,7 @@ img_in = imread('test0.jpg');
 fid_1 = fopen('image_data_in_p1.txt', 'wt'); 
 fid_2 = fopen('image_data_in_p2.txt', 'wt'); 
 fid_3 = fopen('image_data_in_p3.txt', 'wt'); 
-fprintf(fid,'%2x\n ', img_in(:,:,1)); 
-fprintf(fid,'%2x\n ', img_in(:,:,2)); 
-fprintf(fid,'%2x\n ', img_in(:,:,3)); 
+fprintf(fid_1,'%2x\n', img_in(:,:,1)); 
+fprintf(fid_2,'%2x\n', img_in(:,:,2)); 
+fprintf(fid_3,'%2x\n', img_in(:,:,3)); 
 
