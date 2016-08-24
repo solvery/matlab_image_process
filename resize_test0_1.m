1fe1686e2416f0d280d2e986fc4a2d16c94536f2
solvery@163.com <solvery@163.com>
2016-08-24 09:47:18 +0800

    matlab_image_process

--- resize_test0_1.m
+++ resize_test0_1.m
@@ -1,10 +1,15 @@
 
-I = imread('test0.jpg');
-J1 = imresize(I, 1.5, 'nearest');
-J2 = imresize(I, 1.5, 'bilinear');
-J3 = imresize(I, 1.5, 'bicubic');
+I = imread('lena.jpg');
+s=3.3;
+J1 = imresize(I, s, 'nearest');
+J2 = imresize(I, s, 'bilinear');
+J3 = imresize(I, s, 'bicubic');
+J4 = imresize(I, s, 'box');
+J5 = imresize(I, s, 'lanczos2');
 figure, imshow(I)
 figure, imshow(J1)
 figure, imshow(J2)
 figure, imshow(J3)
+figure, imshow(J4)
+figure, imshow(J5)
 
