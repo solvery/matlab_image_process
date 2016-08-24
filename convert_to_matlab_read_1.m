6af63332db1dc6300d473033cd6f3a4386fda605
solvery@163.com <solvery@163.com>
2016-08-24 10:51:23 +0800

    matlab_image_process

--- convert_to_matlab_read_1.m
+++ convert_to_matlab_read_1.m
@@ -1,6 +1,15 @@
 
 data_in_hex_p1 = textread('image_data_in_p1.txt', '%s');  
-data_in_hex_p1 = textread('image_data_in_p1.txt', '%s');  
-data_in_hex_p1 = textread('image_data_in_p1.txt', '%s');  
-data_in_dec = uint8(hex2dec(data_in_hex));
+data_in_hex_p2 = textread('image_data_in_p2.txt', '%s');  
+data_in_hex_p3 = textread('image_data_in_p3.txt', '%s');  
+data_in_dec_p1 = uint8(hex2dec(data_in_hex_p1));
+data_in_dec_p2 = uint8(hex2dec(data_in_hex_p2));
+data_in_dec_p3 = uint8(hex2dec(data_in_hex_p3));
 
+img_1(:,:,1) = data_in_dec_p1;
+img_1(:,:,2) = data_in_dec_p2;
+img_1(:,:,3) = data_in_dec_p3;
+
+%img_1 = im2uint8(img_1);
+figure; 
+imshow(img_1);
