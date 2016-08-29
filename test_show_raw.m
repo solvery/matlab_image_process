2b04d132115fb978710aa37287f78dba50292774
solvery@163.com <solvery@163.com>
2016-08-29 11:23:02 +0800

    add

--- test_show_raw.m
+++ test_show_raw.m
@@ -1,5 +1,14 @@
 
 
-show_raw('lenna_512x512.raw', 512, 512);
+%show_raw('lenna_512x512.raw', 512, 512);
 %show_raw('image_to_raw_1.txt', 6, 8);
+
+%show_raw('/workspace/tmp/./video_stream_scaler/sim/sim_qsta/out/output640x512to1280x1024.raw', 1280, 1024);
+%show_raw('/workspace/tmp/./video_stream_scaler/sim/sim_qsta/src/input640x512RGB.raw', 640, 512);
+show_raw('/workspace/tmp/video_stream_scaler/sim/sim_qsta/out/outputlenna_1024x1024.raw', 1024, 1024);
+show_raw('/workspace/tmp/bilinear_demosaic/./trunk/sim/rtl_sim/src/input640x512RGBBlur.raw', 640, 512);
+show_raw('/workspace/tmp/bilinear_demosaic/trunk/sim/sim_qsta/outputFS.raw',  640, 512);
+
+show_raw('/prj/case.image_scaler/video_stream_scaler/sim/sim_qsta/src/lenna_512x512.raw', 512, 512);
+show_raw('/prj/case.image_scaler/video_stream_scaler/sim/sim_qsta/out/outputlenna_1024x1024.raw', 1024, 1024);
 
