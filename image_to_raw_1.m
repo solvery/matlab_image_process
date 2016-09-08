ad306f72fe9eb117bf8efe69beb53d034111cf91
solvery@163.com <solvery@163.com>
2016-09-08 09:30:38 +0800

    matlab_image_process

--- image_to_raw_1.m
+++ image_to_raw_1.m
@@ -1,16 +1,16 @@
 clc;
 clear;
 
-img_in = imread('test.bmp')
+img_in = imread('test_512x512.bmp');
 %figure, imshow(img_in)
 fid_1 = fopen('image_to_raw_1.txt', 'wt'); 
 
 k=1;
-for i = 1:6
-	for j = 1:8
-		img_raw(k) 	= img_in(7-i,j,3);
-		img_raw(k+1)= img_in(7-i,j,2);
-		img_raw(k+2)= img_in(7-i,j,1);
+for i = 1:512
+	for j = 1:512
+		img_raw(k) 	= img_in(513-i,j,3);
+		img_raw(k+1)= img_in(513-i,j,2);
+		img_raw(k+2)= img_in(513-i,j,1);
 		k=k+3;
 	end
 end
