06e0b0c347d69095ac2d3fcde8d70e874da6c636
solvery@163.com <solvery@163.com>
2016-10-19 20:22:06 +0800

    raw_1

---
+++ raw_1/gen.m
@@ -0,0 +1,16 @@
+
+raw_to_image_1('raw_out_00', 'img_1920x1080_00.jpg', 1920, 1080);
+raw_to_image_1('raw_out_01', 'img_1920x1080_01.jpg', 1920, 1080);
+raw_to_image_1('raw_out_02', 'img_1920x1080_01.jpg', 1920, 1080);
+raw_to_image_1('raw_out_03', 'img_1920x1080_03.jpg', 1920, 1080);
+raw_to_image_1('raw_out_04', 'img_1920x1080_04.jpg', 1920, 1080);
+raw_to_image_1('raw_out_05', 'img_1920x1080_05.jpg', 1920, 1080);
+raw_to_image_1('raw_out_06', 'img_1920x1080_06.jpg', 1920, 1080);
+raw_to_image_1('raw_out_07', 'img_1920x1080_07.jpg', 1920, 1080);
+raw_to_image_1('raw_out_08', 'img_1920x1080_08.jpg', 1920, 1080);
+raw_to_image_1('raw_out_09', 'img_1920x1080_09.jpg', 1920, 1080);
+raw_to_image_1('raw_out_10', 'img_1920x1080_10.jpg', 1920, 1080);
+raw_to_image_1('raw_out_11', 'img_1920x1080_11.jpg', 1920, 1080);
+raw_to_image_1('raw_out_12', 'img_1920x1080_12.jpg', 1920, 1080);
+raw_to_image_1('raw_out_13', 'img_1920x1080_13.jpg', 1920, 1080);
+
---
+++ raw_1/test.m
@@ -0,0 +1,4 @@
+
+raw_to_image_1('raw_out_02', 'img_1920x1080_02.jpg', 1920, 1080);
+raw_to_image_1('raw_out_03', 'img_1920x1080_03.jpg', 1920, 1080);
+
