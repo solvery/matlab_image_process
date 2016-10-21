d117280faf940ac272fa8b1b392b9a57bb453e74
solvery@163.com <solvery@163.com>
2016-10-21 15:44:31 +0800

    raw_1

---
+++ raw_1/gen_image_to_raw_1.m
@@ -0,0 +1,10 @@
+
+image_to_raw_1('raw_in/raw_in_00.jpg', 'raw_in/raw_in_00');
+image_to_raw_1('raw_in/raw_in_01.jpg', 'raw_in/raw_in_01');
+image_to_raw_1('raw_in/raw_in_02.jpg', 'raw_in/raw_in_02');
+image_to_raw_1('raw_in/raw_in_03.jpg', 'raw_in/raw_in_03');
+image_to_raw_1('raw_in/raw_in_04.jpg', 'raw_in/raw_in_04');
+image_to_raw_1('raw_in/raw_in_05.jpg', 'raw_in/raw_in_05');
+image_to_raw_1('raw_in/raw_in_06.jpg', 'raw_in/raw_in_06');
+image_to_raw_1('raw_in/raw_in_07.jpg', 'raw_in/raw_in_07');
+
---
+++ raw_1/gen_raw_in_to_jpg_1.m
@@ -0,0 +1,2 @@
+
+raw_to_image_1('raw_in/raw_in_00', 'raw_in/raw_in_00.jpg', 1920, 1080);
