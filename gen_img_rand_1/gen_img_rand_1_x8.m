5991f63f953652d72dacb26c7fb84709fe10d67b
solvery@163.com <solvery@163.com>
2016-10-21 15:44:37 +0800

    gen_img_rand_1

---
+++ gen_img_rand_1/gen_img_rand_1_x8.m
@@ -0,0 +1,9 @@
+
+gen_img_rand_1(16,9,120, 'raw_in/raw_in_00.jpg');
+gen_img_rand_1(16,9,120, 'raw_in/raw_in_01.jpg');
+gen_img_rand_1(16,9,120, 'raw_in/raw_in_02.jpg');
+gen_img_rand_1(16,9,120, 'raw_in/raw_in_03.jpg');
+gen_img_rand_1(16,9,120, 'raw_in/raw_in_04.jpg');
+gen_img_rand_1(16,9,120, 'raw_in/raw_in_05.jpg');
+gen_img_rand_1(16,9,120, 'raw_in/raw_in_06.jpg');
+gen_img_rand_1(16,9,120, 'raw_in/raw_in_07.jpg');
---
+++ gen_img_rand_1/test.m
@@ -0,0 +1,2 @@
+
+gen_img_rand_1(16,9,120, 'raw_in/raw_in_00.jpg');
