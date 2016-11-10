effc8d232266102f319db08a92364405c6a12e0a
solvery@163.com <solvery@163.com>
2016-11-10 15:07:15 +0800

    /home/yang/workspace/verdiLog/matlab_image_process/video_1 at video_1

--- video_1/video_2.m
+++ video_1/video_2.m
@@ -1,11 +1,11 @@
 
-v = VideoWriter('video_1.avi');
-v.FrameRate = 0.1;
-
+v = VideoWriter('video_2.avi');
+v.FrameRate = 1;
 
 open(v);
 for k = 1:60
-	writeVideo(v,rand(1080,1920));
+    img_1 = gen_img_rand_1(16,9,120);
+	writeVideo(v, img_1);
 end
 close(v);
 
