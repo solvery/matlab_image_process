58d62580deb6434f68111e0065178e812ad01d8d
solvery@163.com <solvery@163.com>
2016-11-10 16:36:37 +0800

    /home/yang/workspace/verdiLog/matlab_image_process/video_1 at video_1

--- video_1/gen1.sh
+++ video_1/gen1.sh
@@ -1,5 +1,6 @@
 alias mrun='matlab -nodisplay -nodesktop -nojvm -nosplash -r'
 date=$(date +%F_%H.%M.%S)
+
 video="video_2"
 f=1  ;m=30 ; mrun "$video($f,$m); exit" && mv $video.avi $video\_f$f\_m$m\_$date.avi
 f=2  ;m=30 ; mrun "$video($f,$m); exit" && mv $video.avi $video\_f$f\_m$m\_$date.avi
