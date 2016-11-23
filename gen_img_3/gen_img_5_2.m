d9aa5431b24e625bdb5d878852da1d55fe270975
solvery@163.com <solvery@163.com>
2016-11-23 19:26:14 +0800

    /home/yang/workspace/verdiLog/matlab_image_process/gen_img_3 at gen_img_3

--- gen_img_3/gen_img_5_2.m
+++ gen_img_3/gen_img_5_2.m
@@ -1,15 +1,15 @@
 
 function X = gen_img_5;
 
-    img_1 = rand(1080, 1920);
+    img_1 = rand(1080, 1920, 3);
 
 for pos = 0:120:1920
     img_2 = gen_img_3y(img_1, pos);
-    fn_out = ['rand1_1080_y' num2str(pos,'%04d') '.bmp'];
+    fn_out = ['rand3_1080_y' num2str(pos,'%04d') '.bmp'];
     imwrite(img_2, fn_out);
 end
 for pos = 0:120:1080
     img_2 = gen_img_3x(img_1, pos);
-    fn_out = ['rand1_1080_x' num2str(pos,'%04d') '.bmp'];
+    fn_out = ['rand3_1080_x' num2str(pos,'%04d') '.bmp'];
     imwrite(img_2, fn_out);
 end
