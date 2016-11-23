4ed3559f07ccf641cb2a05871fce120c1c76700a
solvery@163.com <solvery@163.com>
2016-11-23 19:43:11 +0800

    /home/yang/workspace/verdiLog/matlab_image_process/gen_img_3_r at gen_img_3_r

--- gen_img_3_r/gen_img_5.m
+++ gen_img_3_r/gen_img_5.m
@@ -3,12 +3,12 @@ function X = gen_img_5;
 
     img_1 = gen_img_2(1080, 1920);
 
-for pos = 0:120:1920
+for pos = 1:120:1920
     img_2 = gen_img_3y(img_1, pos);
     fn_out = ['colorbar_1080_y' num2str(pos,'%04d') '.bmp'];
     imwrite(img_2, fn_out);
 end
-for pos = 0:120:1080
+for pos = 1:120:1080
     img_2 = gen_img_3x(img_1, pos);
     fn_out = ['colorbar_1080_x' num2str(pos,'%04d') '.bmp'];
     imwrite(img_2, fn_out);
--- gen_img_3_r/gen_img_5_1.m
+++ gen_img_3_r/gen_img_5_1.m
@@ -3,12 +3,12 @@ function X = gen_img_5;
 
     img_1 = rand(1080, 1920);
 
-for pos = 0:120:1920
+for pos = 1:120:1920
     img_2 = gen_img_3y(img_1, pos);
     fn_out = ['rand1_1080_y' num2str(pos,'%04d') '.bmp'];
     imwrite(img_2, fn_out);
 end
-for pos = 0:120:1080
+for pos = 1:120:1080
     img_2 = gen_img_3x(img_1, pos);
     fn_out = ['rand1_1080_x' num2str(pos,'%04d') '.bmp'];
     imwrite(img_2, fn_out);
