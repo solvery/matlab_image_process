8f56ca9bbe88494559ab74a7ba1d66552a87a43e
solvery@163.com <solvery@163.com>
2016-11-23 19:12:01 +0800

    /home/yang/workspace/verdiLog/matlab_image_process/gen_img_3 at gen_img_3

---
+++ gen_img_3/gen_video_3x.m
@@ -0,0 +1,15 @@
+
+function gen_video_1x(frame_rate, step)
+
+fn_out = ['video_rand1_1080_x_s' num2str(step, '%03d') '_f' num2str(frame_rate,'%02d') ];
+v = VideoWriter(fn_out);
+v.FrameRate = frame_rate;
+open(v);
+
+img_1 = rand(1080, 1920);
+
+for pos = 0:step:1080
+    img_2 = gen_img_3x(img_1, pos);
+	writeVideo(v, img_2);
+end
+
--- gen_img_3/run.sh
+++ gen_img_3/run.sh
@@ -1,5 +1,14 @@
 alias mrun='matlab -nodisplay -nodesktop -nojvm -nosplash -r'
 
-mrun "gen_video_1(1,120)" &
-mrun "gen_video_1(2,120)" &
+mrun "gen_video_2x(1,120)" &
+mrun "gen_video_2y(1,120)" &
 
+mrun "gen_video_2x(1,1)" &
+mrun "gen_video_2y(1,1)" &
+mrun "gen_video_2x(30,1)" &
+mrun "gen_video_2y(30,1)" &
+mrun "gen_video_2x(60,1)" &
+mrun "gen_video_2y(60,1)" &
+
+mrun "gen_video_2x(1,12)" &
+mrun "gen_video_2y(1,12)" &
