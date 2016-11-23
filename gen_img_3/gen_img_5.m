6d98d5170fc6d6fc3d425558d745497dfb5615d3
solvery@163.com <solvery@163.com>
2016-11-23 17:42:20 +0800

    /home/yang/workspace/verdiLog/matlab_image_process/gen_img_3 at gen_img_3

--- gen_img_3/gen_img_5.m
+++ gen_img_3/gen_img_5.m
@@ -1,9 +1,15 @@
 
-function X = gen_img_4(x);
+function X = gen_img_5;
 
     img_1 = gen_img_2(1080, 1920);
-    img_2 = gen_img_3(img_1, 1, x);
-    fn_out = ['colorbar_1080_x' num2str(x,'%04d') '.bmp'];
+
+for pos = 0:120:1920
+    img_2 = gen_img_3y(img_1, pos);
+    fn_out = ['colorbar_1080_y' num2str(pos,'%04d') '.bmp'];
     imwrite(img_2, fn_out);
-
-
+end
+for pos = 0:120:1080
+    img_2 = gen_img_3x(img_1, pos);
+    fn_out = ['colorbar_1080_x' num2str(pos,'%04d') '.bmp'];
+    imwrite(img_2, fn_out);
+end
--- gen_img_3/test.m
+++ gen_img_3/test.m
@@ -1,5 +1,2 @@
 
 gen_img_4(100);
-gen_img_5(100);
-gen_img_4(1000);
-gen_img_5(1000);
