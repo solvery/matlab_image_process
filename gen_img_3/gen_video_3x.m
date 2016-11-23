a5501dfc890c4c5895403e0c2e00cb6fb7f59d79
solvery@163.com <solvery@163.com>
2016-11-23 19:12:38 +0800

    /home/yang/workspace/verdiLog/matlab_image_process/gen_img_3 at gen_img_3

--- gen_img_3/gen_video_3x.m
+++ gen_img_3/gen_video_3x.m
@@ -1,12 +1,12 @@
 
 function gen_video_1x(frame_rate, step)
 
-fn_out = ['video_rand1_1080_x_s' num2str(step, '%03d') '_f' num2str(frame_rate,'%02d') ];
+fn_out = ['video_rand3_1080_x_s' num2str(step, '%03d') '_f' num2str(frame_rate,'%02d') ];
 v = VideoWriter(fn_out);
 v.FrameRate = frame_rate;
 open(v);
 
-img_1 = rand(1080, 1920);
+img_1 = rand(1080, 1920, 3);
 
 for pos = 0:step:1080
     img_2 = gen_img_3x(img_1, pos);
--- gen_img_3/gen_video_3y.m
+++ gen_img_3/gen_video_3y.m
@@ -1,12 +1,12 @@
 
 function gen_video_1y(frame_rate, step)
 
-fn_out = ['video_rand1_1080_y_s' num2str(step, '%03d') '_f' num2str(frame_rate,'%02d') ];
+fn_out = ['video_rand3_1080_y_s' num2str(step, '%03d') '_f' num2str(frame_rate,'%02d') ];
 v = VideoWriter(fn_out);
 v.FrameRate = frame_rate;
 open(v);
 
-img_1 = rand(1080, 1920);
+img_1 = rand(1080, 1920, 3);
 
 for pos = 0:step:1920
     img_2 = gen_img_3y(img_1, pos);
