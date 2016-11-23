efce99d7b06c119ad5cc4c368321f59817b0f1c8
solvery@163.com <solvery@163.com>
2016-11-23 19:14:31 +0800

    /home/yang/workspace/verdiLog/matlab_image_process/gen_img_3 at gen_img_3

--- gen_img_3/run.sh
+++ gen_img_3/run.sh
@@ -1,5 +1,19 @@
 alias mrun='matlab -nodisplay -nodesktop -nojvm -nosplash -r'
 
+mrun "gen_video_1x(1,120)" &
+mrun "gen_video_1y(1,120)" &
+
+mrun "gen_video_1x(1,1)" &
+mrun "gen_video_1y(1,1)" &
+mrun "gen_video_1x(30,1)" &
+mrun "gen_video_1y(30,1)" &
+mrun "gen_video_1x(60,1)" &
+mrun "gen_video_1y(60,1)" &
+
+mrun "gen_video_1x(1,12)" &
+mrun "gen_video_1y(1,12)" &
+
+
 mrun "gen_video_2x(1,120)" &
 mrun "gen_video_2y(1,120)" &
 
@@ -12,3 +26,18 @@ mrun "gen_video_2y(60,1)" &
 
 mrun "gen_video_2x(1,12)" &
 mrun "gen_video_2y(1,12)" &
+
+
+mrun "gen_video_3x(1,120)" &
+mrun "gen_video_3y(1,120)" &
+
+mrun "gen_video_3x(1,1)" &
+mrun "gen_video_3y(1,1)" &
+mrun "gen_video_3x(30,1)" &
+mrun "gen_video_3y(30,1)" &
+mrun "gen_video_3x(60,1)" &
+mrun "gen_video_3y(60,1)" &
+
+mrun "gen_video_3x(1,12)" &
+mrun "gen_video_3y(1,12)" &
+
