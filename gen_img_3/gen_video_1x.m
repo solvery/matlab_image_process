58f5c4e179e9da29939d411e1b2144c97a5b5e0b
solvery@163.com <solvery@163.com>
2016-11-24 11:50:24 +0800

    /home/yang/workspace/verdiLog/matlab_image_process/gen_img_3 at gen_img_3

--- gen_img_3/gen_video_1x.m
+++ gen_img_3/gen_video_1x.m
@@ -1,7 +1,7 @@
 
 function gen_video_1x(frame_rate, step)
 
-fn_out = ['video_colorbar_blank_1080_x_s' num2str(step, '%03d') '_f' num2str(frame_rate,'%02d') ];
+fn_out = ['video_colorbar_1080_x_s' num2str(step, '%03d') '_f' num2str(frame_rate,'%02d') ];
 v = VideoWriter(fn_out);
 v.FrameRate = frame_rate;
 open(v);
