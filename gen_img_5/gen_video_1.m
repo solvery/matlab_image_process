ec9b32054b5e61440510d568f5f9e98340793738
solvery@163.com <solvery@163.com>
2016-11-24 15:03:05 +0800

    /home/yang/workspace/verdiLog/matlab_image_process/gen_img_5 at gen_img_5

--- gen_img_5/gen_video_1.m
+++ gen_img_5/gen_video_1.m
@@ -1,53 +1,17 @@
 
-function gen_video_1(img_1, method, step, frame_rate)
+function gen_video_1;
+
+img_1 = imread('DisplayX/img_01.bmp');
+img_2 = imread('DisplayX/img_02.bmp');
+method = 'high_contrast01_'
+frame_rate=1
 
 fn_out = [method '_1080_f' num2str(frame_rate,'%02d') ];
 video_1 = VideoWriter(fn_out);
 video_1.FrameRate = frame_rate;
 open(video_1);
 
-for pos = step:step:1920
-    img_2 = img_1;
-    img_2(:,1:pos,:) = 0;
+for pos = 1:10
+    writeVideo(video_1, img_1);
     writeVideo(video_1, img_2);
 end
-for pos = 1920:-step:step
-    img_2 = img_1;
-    img_2(:,1:pos,:) = 0;
-    writeVideo(video_1, img_2);
-end
-
-
-for pos = step:step:1920
-    img_2 = img_1;
-    img_2(:,pos:end,:) = 0;
-    writeVideo(video_1, img_2);
-end
-for pos = 1920:-step:step
-    img_2 = img_1;
-    img_2(:,pos:end,:) = 0;
-    writeVideo(video_1, img_2);
-end
-
-for pos = step:step:1080
-    img_2 = img_1;
-    img_2(1:pos,:,:) = 0;
-    writeVideo(video_1, img_2);
-end
-for pos = 1080:-step:step
-    img_2 = img_1;
-    img_2(1:pos,:,:) = 0;
-    writeVideo(video_1, img_2);
-end
-
-for pos = step:step:1080
-    img_2 = img_1;
-    img_2(pos:end,:,:) = 0;
-    writeVideo(video_1, img_2);
-end
-for pos = 1080:-step:step
-    img_2 = img_1;
-    img_2(pos:end,:,:) = 0;
-    writeVideo(video_1, img_2);
-end
-
