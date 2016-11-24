75e8a8d38326b1584254e4aed56ab0af2f9fdc54
solvery@163.com <solvery@163.com>
2016-11-24 15:04:24 +0800

    /home/yang/workspace/verdiLog/matlab_image_process/gen_img_5 at gen_img_5

--- gen_img_5/gen_video_1.m
+++ gen_img_5/gen_video_1.m
@@ -3,7 +3,7 @@ function gen_video_1;
 
 img_1 = imread('DisplayX/img_01.bmp');
 img_2 = imread('DisplayX/img_02.bmp');
-method = 'high_contrast01_'
+method = 'high_contrast01'
 frame_rate=1
 
 fn_out = [method '_1080_f' num2str(frame_rate,'%02d') ];
