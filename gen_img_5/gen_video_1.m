c45457dab8e8684eeca487884498d9f37e6b9435
solvery@163.com <solvery@163.com>
2016-11-24 15:17:35 +0800

    /home/yang/workspace/verdiLog/matlab_image_process/video_1 at video_1

--- gen_img_5/gen_video_1.m
+++ gen_img_5/gen_video_1.m
@@ -7,7 +7,7 @@ video_1 = VideoWriter(fn_out);
 video_1.FrameRate = frame_rate;
 open(video_1);
 
-for pos = 1:10
+for pos = 1:(10*frame_rate)
     writeVideo(video_1, img_1);
     writeVideo(video_1, img_2);
 end
